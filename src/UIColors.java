5ad71d5339343a345e66cc9ae298d2f6332eb5df
aagamaar <aagamaar01@gmail.com>
2025-10-18 01:02:32 +0530

    Added UIColors.java

---
+++ src/UIColors.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class UIColors {
+    public static final Color PRIMARY_RED = new Color(204, 0, 0);
+    public static final Color SECONDARY_RED = new Color(255, 102, 102);
+    public static final Color LIGHT_BACKGROUND = new Color(255, 245, 245);
+    public static final Color ACCENT_BLUE = new Color(0, 102, 204);
+    public static final Color SUCCESS_GREEN = new Color(0, 153, 0);
+    public static final Color WARNING_ORANGE = new Color(255, 153, 0);
+}
